56d3482d1bdeefc9e5dd50d0b9ee8db3e78d52c9
bajinsheng <43855029@qq.com>
2021-03-15 15:47:15 +0800

    update seed

--- sample/applications/run.sh
+++ sample/applications/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 4 ]; then
   echo "./run.sh targe_program unique_shadow_int_value afl_mode (e.g., ./run.sh objdump 3 2)"
   echo "- afl_mode (1: normal mode, 2: dynamic_switch mode)"
   echo "please copy this script into target testing folder (e.g., cp ./run.sh /build_asan)"
   exit 1
 fi
 
-mkdir -p ./fuzz/$1/output
-rm -rf ./fuzz/$1/output/*
+mkdir -p ./fuzz/$1/$4
+rm -rf ./fuzz/$1/$4/*
 
 sudo sh -c 'echo core | sudo tee /proc/sys/kernel/core_pattern'
 sudo sh -c 'cd /sys/devices/system/cpu; echo performance | sudo tee cpu*/cpufreq/scaling_governor; cd -'
@@ -43,10 +43,10 @@ then
   export MINSHADOW_PATH_16G="$(pwd)/../build_min-16G/$1/prelink-$1/$1"
 if [ $3 == 2 ]; then
   export MINMODE_ON="1"
-  ../../../afl/afl-2.52b/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./$1/prelink-$1/$1 -n -e -r  @@
+  ../../../afl/afl-2.52b/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./$1/prelink-$1/$1 -n -e -r  @@
 else
   export MINMODE_ON="0"
-  ../../../afl/afl-2.52b-native/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./$1/$1 -n -e -r  @@
+  ../../../afl/afl-2.52b-native/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./$1/$1 -n -e -r  @@
 fi
 
 elif [ $1 == "gif2png" ]
@@ -60,10 +60,10 @@ then
   export MINSHADOW_PATH_16G="$(pwd)/../build_min-16G/gif2png/prelink-$1/$1"
 if [ $3 == 2 ]; then
   export MINMODE_ON="1"
-  ../../../afl/afl-2.52b/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./gif2png/prelink-$1/$1  @@
+  ../../../afl/afl-2.52b/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./gif2png/prelink-$1/$1  @@
 else
   export MINMODE_ON="0"
-  ../../../afl/afl-2.52b-native/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./gif2png/$1  @@
+  ../../../afl/afl-2.52b-native/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./gif2png/$1  @@
 fi
 
 elif [ $1 == "pngfix" ]
@@ -77,10 +77,10 @@ then
   export MINSHADOW_PATH_16G="$(pwd)/../build_min-16G/libpng/prelink-pngfix/$1"
 if [ $3 == 2 ]; then
   export MINMODE_ON="1"
-  ../../../afl/afl-2.52b/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./libpng/prelink-$1/$1  @@
+  ../../../afl/afl-2.52b/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./libpng/prelink-$1/$1  @@
 else
   export MINMODE_ON="0"
-  ../../../afl/afl-2.52b-native/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./libpng/build/bin/$1  @@
+  ../../../afl/afl-2.52b-native/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./libpng/build/bin/$1  @@
 fi
 
 elif [ $1 == "ffmpeg" ]
@@ -94,10 +94,10 @@ then
   export MINSHADOW_PATH_16G="$(pwd)/../build_min-16G/ffmpeg/ffmpeg_AV_CODEC_ID_MPEG4_fuzzer"
 if [ $3 == 2 ]; then
   export MINMODE_ON="1"
-  ../../../afl/afl-2.52b/afl-fuzz -t 5000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./ffmpeg/ffmpeg_AV_CODEC_ID_MPEG4_fuzzer @@
+  ../../../afl/afl-2.52b/afl-fuzz -t 5000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./ffmpeg/ffmpeg_AV_CODEC_ID_MPEG4_fuzzer @@
 else
   export MINMODE_ON="0"
-  ../../../afl/afl-2.52b-native/afl-fuzz -t 5000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./ffmpeg/ffmpeg_AV_CODEC_ID_MPEG4_fuzzer @@
+  ../../../afl/afl-2.52b-native/afl-fuzz -t 5000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./ffmpeg/ffmpeg_AV_CODEC_ID_MPEG4_fuzzer @@
 fi
 
 elif [ $1 == "file" ]
@@ -111,35 +111,35 @@ then
   export MINSHADOW_PATH_16G="$(pwd)/../build_min-16G/$1/prelink-$1/$1"
 if [ $3 == 2 ]; then
   export MINMODE_ON="1"
-  ../../../afl/afl-2.52b/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./$1/prelink-$1/$1 -m ./$1/magic/magic.mgc @@
+  ../../../afl/afl-2.52b/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./$1/prelink-$1/$1 -m ./$1/magic/magic.mgc @@
 else
   export MINMODE_ON="0"
-  ../../../afl/afl-2.52b-native/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./$1/file -m ./$1/magic/magic.mgc @@
+  ../../../afl/afl-2.52b-native/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./$1/file -m ./$1/magic/magic.mgc @@
 fi
 elif [ $1 == "objdump" ]
 then
 if [ $3 == 2 ]; then
   export MINMODE_ON="1"
-  ../../../afl/afl-2.52b/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./binutils/prelink-$1/$1 -a @@
+  ../../../afl/afl-2.52b/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./binutils/prelink-$1/$1 -a @@
 else
   export MINMODE_ON="0"
-  ../../../afl/afl-2.52b-native/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./binutils/$1 -a @@
+  ../../../afl/afl-2.52b-native/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./binutils/$1 -a @@
 fi
 elif [ $1 == "c++filt" ]
 then
 if [ $3 == 2 ]; then
   export MINMODE_ON="1"
-  ../../../afl/afl-2.52b/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./binutils/prelink-$1/$1 -n
+  ../../../afl/afl-2.52b/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./binutils/prelink-$1/$1 -n
 else
   export MINMODE_ON="0"
-  ../../../afl/afl-2.52b-native/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./binutils/$1 -n
+  ../../../afl/afl-2.52b-native/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./binutils/$1 -n
 fi
 else
 if [ $3 == 2 ]; then
   export MINMODE_ON="1"
-  ../../../afl/afl-2.52b/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./binutils/prelink-$1/$1 @@
+  ../../../afl/afl-2.52b/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./binutils/prelink-$1/$1 @@
 else
   export MINMODE_ON="0"
-  ../../../afl/afl-2.52b-native/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/output -m none -- ./binutils/$1 @@
+  ../../../afl/afl-2.52b-native/afl-fuzz -t 1000 -i ../full-seeds/$1 -o ./fuzz/$1/$4 -m none -- ./binutils/$1 @@
 fi
 fi
